0518a0309d641c7b2833203b838dbb43aa7ceb08
Lucas dos Santos <lucas6eng@gmail.com>
2025-07-15 20:40:32 +0200

    add reload

--- scenes/camera/camera.gd
+++ scenes/camera/camera.gd
@@ -5,7 +5,7 @@ extends Camera2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_limit(SIDE_TOP, 0)
-
+	global_position.x = pip.global_position.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scenes/level1/level1.gd
+++ scenes/level1/level1.gd
@@ -10,3 +10,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		get_tree().reload_current_scene()
