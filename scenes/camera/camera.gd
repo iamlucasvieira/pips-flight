0d052e67dcb0a29310f311e417ae312047a68a23
Lucas dos Santos <lucas6eng@gmail.com>
2025-07-17 17:25:06 +0200

    Allow level to grow vertically

--- scenes/camera/camera.gd
+++ scenes/camera/camera.gd
@@ -2,11 +2,13 @@ extends Camera2D
 
 @export var pip: Player
 
+var viewport_height: int = ProjectSettings.get_setting("display/window/size/viewport_height")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_limit(SIDE_TOP, 0)
-	global_position.x = pip.global_position.x
+	set_limit(SIDE_BOTTOM, viewport_height)
+	global_position = pip.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	global_position.x = pip.global_position.x
+	global_position = pip.global_position
