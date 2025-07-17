cf7a984b1f9715ed9f2a1475cfbc6f946d36fa49
Lucas dos Santos <lucas6eng@gmail.com>
2025-07-17 17:15:53 +0200

    Make button work

---
+++ scenes/title/button.gd
@@ -0,0 +1,17 @@
+extends Button
+
+const LEVEL_1 = preload("res://scenes/level1/level1.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_packed(LEVEL_1)
+	
